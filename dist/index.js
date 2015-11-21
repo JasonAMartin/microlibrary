1d682349c22ffef1dc318cf14b34000cae5e3dd3
Jason A. Martin <jmartin@jasonamartin.com>
2015-11-20 23:06:18 -0800

    bug: added dist folder and fixed .gitignore + package.json

---
+++ dist/index.js
@@ -0,0 +1,20 @@
+'use strict';
+
+var _uniqueRandomArray = require('unique-random-array');
+
+var _uniqueRandomArray2 = _interopRequireDefault(_uniqueRandomArray);
+
+var _gameNames = require('./game-names.json');
+
+var _gameNames2 = _interopRequireDefault(_gameNames);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+module.exports = {
+  all: _gameNames2.default,
+  random: (0, _uniqueRandomArray2.default)(_gameNames2.default)
+};
+
+var blue = function blue(something) {
+  console.log("hey");
+};
